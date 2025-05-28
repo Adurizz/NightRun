07642f4376648ffddc68a7abb1cbec8f17b0e59e
Adurizz <90387438+Adurizz@users.noreply.github.com>
2025-05-28 11:01:04 +0900

    Text 등 에셋 추가

---
+++ Assets/02_Scripts/TestingScriptForBtn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScriptForBtn : MonoBehaviour
+{
+    public void MyMethod()
+    {
+
+    }
+
+    public void MyMethodNeedsParam(int i)
+    {
+
+    }
+}
